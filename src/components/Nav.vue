d82c277ea181277afb63aefbf10d2124f376fbb2
Rodolfo Roman <rromanv@gmail.com>
2021-11-11 00:45:40 -0600

    Ended Main Page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="text-green-200 bg-green-800">
     <div class="container flex items-center justify-center mx-auto">
-      <h1 class="py-8 text-3xl font-thin tracking-tighter">
-        Advice<span class="font-normal">Call</span>
-      </h1>
+      <router-link to="/">
+        <h1 class="py-8 text-3xl font-thin tracking-tighter">
+          LOTR<span class="font-normal">Movies</span>
+        </h1></router-link
+      >
     </div>
   </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,26 @@
 <script setup>
-import { ref } from "vue";
-import { useAdvice } from "../composables/useAdvice";
+import { useAPI } from "../composables/useAPI";
 
-const { advices, search } = useAdvice();
-
-const searchItem = ref("");
+const { movies, getMovies } = useAPI();
 </script>
 
 <template>
-  <div>
-    <input
-      v-model="searchItem"
-      @change="search(searchItem)"
-      type="text"
-      placeholder="Search an Advise..."
-      class="w-full py-4 mt-16 text-xl text-center rounded-full"
-    />
-    <div class="grid grid-cols-2 gap-16 pt-16">
-      <div v-for="(advice, index) in advices" :key="index">
-        <p>
-          {{ advice.advice }}
-        </p>
-      </div>
-    </div>
+  <div class="mt-16">
+    <ul class="grid grid-cols-3 gap-12">
+      <router-link
+        class="p-4 bg-white rounded-lg shadow-2xl cursor-pointer  hover:scale-105"
+        v-for="movie in movies"
+        :key="movie._id"
+        :to="`/movie/${movie._id}`"
+      >
+        <div>
+          <h3 class="text-xl font-semibold tracking-tight">{{ movie.name }}</h3>
+          <p>Runtime: {{ movie.runtimeInMinutes }} minutes</p>
+          <p>Budget in Millions: ${{ movie.budgetInMillions }}</p>
+          <p>Academic Awards: {{ movie.academyAwardWins }}</p>
+          <p>Rotten Tomatoes: {{ movie.rottenTomatoesScore }}</p>
+        </div>
+      </router-link>
+    </ul>
   </div>
 </template>
