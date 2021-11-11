c0e30a10dd78d009a420e1547c4fabe381a1ab07
Rodolfo Román <rromanv@users.noreply.github.com>
2021-11-11 00:20:27 -0600

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-green-200 bg-green-800">
+    <div class="container flex items-center justify-center mx-auto">
+      <h1 class="py-8 text-3xl font-thin tracking-tighter">
+        Advice<span class="font-normal">Call</span>
+      </h1>
+    </div>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+import { useAdvice } from "../composables/useAdvice";
+
+const { advices, search } = useAdvice();
+
+const searchItem = ref("");
+</script>
+
+<template>
+  <div>
+    <input
+      v-model="searchItem"
+      @change="search(searchItem)"
+      type="text"
+      placeholder="Search an Advise..."
+      class="w-full py-4 mt-16 text-xl text-center rounded-full"
+    />
+    <div class="grid grid-cols-2 gap-16 pt-16">
+      <div v-for="(advice, index) in advices" :key="index">
+        <p>
+          {{ advice.advice }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
